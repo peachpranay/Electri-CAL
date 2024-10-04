d4cabc3a94466788b8a6d6d3446577e72c9fadd1
parthratra11 <parthratra11@gmail.com>
2024-10-04 18:00:00 +0530

    About Us Page Link created

--- Dashboard/electri-cal/app/components/Header/aboutUs.tsx
+++ Dashboard/electri-cal/app/components/Header/aboutUs.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
-const AboutUs = () => {
+
+const AboutUsRedirect = () => {
   return (
     <button className="btn btn-ghost text-lg ml-6 hover:bg-inherit hover:opacity-75 ">
-      About Us
+      <Link href = '/about-us'>
+        About Us
+      </Link>
     </button>
   );
 };
 
-export default AboutUs;
+export default AboutUsRedirect;
--- Dashboard/electri-cal/app/components/Header/electriCal.tsx
+++ Dashboard/electri-cal/app/components/Header/electriCal.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+import Link from "next/link";
 
 const ElectriCal = () => {
   return (
+    
     <div>
-      <h1 className="ml-1 btn btn-ghost text-4xl font-bold font-sans hover:bg-inherit hover:opacity-75">
-        Electri-Cal
-      </h1>
+      <Link href = '/'>
+        <h1 className="ml-1 btn btn-ghost text-4xl font-bold font-sans hover:bg-inherit hover:opacity-75">
+          Electri-Cal
+        </h1>
+      </Link>
     </div>
+    
   );
 };
 
